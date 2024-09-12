2fc7607af428595e7e7002d7c874a54007d333e0
Debarima Basu <151932080+DebarimaBasu@users.noreply.github.com>
2024-09-12 14:24:59 +0530

    Update CustomInput.tsx

--- components/CustomInput.tsx
+++ components/CustomInput.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+/*import React from 'react'
 import { Input } from './ui/input'
 import { Control, FieldPath } from 'react-hook-form'
 import { z } from 'zod'
@@ -33,6 +33,53 @@ const CustomInput = ({ control , name, label, placeholder }:
             </FormControl>
             <FormMessage
               className="form-message mt-2"/>
+           </div>
+          )}
+        />
+    
+  )
+}*/
+
+export default CustomInput
+import React from 'react'
+import { Input } from './ui/input'
+import { Control, FieldPath } from 'react-hook-form'
+import { z } from 'zod'
+import { FormControl, FormField,FormLabel,FormMessage} from './ui/form'
+import { authformSchema } from '@/lib/utils'
+const formSchema = authformSchema('sign-up')
+  interface CustomInput{
+    control:Control<z.infer<typeof formSchema>>,
+    name: FieldPath<z.infer<typeof formSchema>>,
+    label: string,
+    placeholder: string
+  }
+const CustomInput = ({ control , name, label, placeholder }:
+ CustomInput) => {
+  return (
+    
+      <FormField
+          control={control
+            
+          }
+          name= { name }
+          render={({ field }) => (
+           <div className="form-item">
+            <FormLabel className="form-label">
+             {label}
+            </FormLabel>
+            <div className='flex w-full flex-col'>
+            <FormControl>
+              <Input 
+              placeholder={placeholder}
+              className="input-class"
+              type={name==='password' ? 'password' :'text'}
+              {...field}
+              />
+            </FormControl>
+            <FormMessage
+              className="form-message mt-2"/>
+           </div>
            </div>
           )}
         />
